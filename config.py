064213108a62cec75b359a1d0875f2b0a3f6b6a0
slayer <meerutuniversity996@gmail.com>
2026-02-07 19:03:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class config:
     #Client
     API_ID = 26249286
     API_HASH = "4e3bf0b014fda4ac752e8f4ab854279b"
-    BOT_TOKEN = "8574354949:AAECJ0rGRU7XSOIxuPy4SB8pjOSk9dnFb3k"
+    BOT_TOKEN = "8212996914:AAGvwAjyxRx4UicFCWZEzWIVTimUD0jHjDM"
     BOT_NAME = "「 ʀʏᴢᴇɴ ᴋᴜʀᴏᴋᴀᴍɪ 」"
     BOT_USERNAME = "Ryzen_xbot"
     BOT_ID = 8212996914
